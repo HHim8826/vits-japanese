9f5318c42051b48bfd4f6505d72ca5513d4057ab
HHim8826 <hhim8826@gmail.com>
2022-08-15 15:02:24 +0800

    lycoris

--- text/symbols.py
+++ text/symbols.py
@@ -1,2 +1,2 @@
-symbols = list(' !"&*,-.?ABCINU[]abcdefghijklmnoprstuwyz{}~():=x@+v%#/1234567890S')
+symbols = list(' !"&*,-.?ABCINU[]abcdefghijklmnoprstuwyz{}~():=x@+v%#/1234567890SO<>;')
 SPACE_ID = symbols.index(" ")
